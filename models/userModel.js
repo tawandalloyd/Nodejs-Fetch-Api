be73fd3f711208116f689fcda7d88d92354c1ef2
tawandalloyd <tawandalloydcharuka@gmail.com>
2022-07-15 18:07:56 +0200

    api

--- models/userModel.js
+++ models/userModel.js
@@ -6,19 +6,19 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   id: {
     type: Number,
-    required: [true, 'id is required']
+    //required: [true, 'id is required']
   },
   name: {
     type: String,
-    required: [true, 'Please tell us your name!']
+   // required: [true, 'Please tell us your name!']
   },
   username: {
     type: String,
-    required: [true, 'Please tell us your username!']
+    //required: [true, 'Please tell us your username!']
   },
   email: {
     type: String,
-    required: [true, 'Please provide your email'],
+    //required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email']
@@ -26,51 +26,51 @@ const userSchema = new mongoose.Schema({
   address: {
     street: {
       type: String,
-      required: [true, 'Please tell us your address!']
+      //required: [true, 'Please tell us your address!']
     },
     suite: {
       type: String,
-      required: [true, 'Please tell us your suite!']
+      //required: [true, 'Please tell us your suite!']
     },
     city: {
       type: String,
-      required: [true, 'Please tell us your city!']
+      //required: [true, 'Please tell us your city!']
     },
     zipcode: {
       type: String,
-      required: [true, 'Please tell us your zipcode!']
+      //required: [true, 'Please tell us your zipcode!']
     },
     geo: {
       lat: {
         type: String,
-        required: [true, 'Please tell us your latitude!']
+        //required: [true, 'Please tell us your latitude!']
       },
       lng: {
         type: String,
-        required: [true, 'Please tell us your longitude!']
+        //required: [true, 'Please tell us your longitude!']
       },
     },
   },
   phone: {
     type: String,
-    required: [true, 'Please tell us your phone number!']
+    //required: [true, 'Please tell us your phone number!']
   },
   website: {
     type: String,
-    required: [true, 'Please tell us your website!']
+    //required: [true, 'Please tell us your website!']
   },
   company: {
     name: {
       type: String,
-      required: [true, 'Please tell us your company name!']
+      //required: [true, 'Please tell us your company name!']
     },
     catchPhrase: {
       type: String,
-      required: [true, 'Please tell us your catch phrase!']
+      //required: [true, 'Please tell us your catch phrase!']
     },
     bs: {
       type: String,
-      required: [true, 'Please tell us your bs!']
+      //required: [true, 'Please tell us your bs!']
     },
   },
   
