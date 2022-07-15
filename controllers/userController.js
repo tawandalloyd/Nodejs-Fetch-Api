0cf94974ef4161efe01c9fc38ed7cad527a376e7
tawandalloyd <tawandalloydcharuka@gmail.com>
2022-07-15 20:07:24 +0200

    Delete api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,10 @@
 const UserModel = require('../models/userModel');
 const factory = require('./handlerFactory');
+const dev_data = require('./../dev-data/data/import-dev-data');
 
 exports.createUser = factory.createOne(UserModel)
 exports.getUser = factory.getOne(UserModel);
 exports.getAllUsers = factory.getAll(UserModel);
 exports.deleteUser = factory.deleteOne(UserModel);
 exports.updateUser = factory.updateOne(UserModel);
+
